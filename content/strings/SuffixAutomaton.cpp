c2ed2f817f56c05210d87760aaccd83fea497f48
Anton Stepanov <pandapythoner@gmail.com>
2025-10-27 20:43:49 +0300

    Flex

--- content/strings/SuffixAutomaton.cpp
+++ content/strings/SuffixAutomaton.cpp
@@ -1,16 +1,15 @@
 /**
- * Author: Igor Markelov (stole from emaxx-algo)
- * Date: 2022-11-18
+ * Author: Anton Stepanov
+ * Date: 2025-10-27
  * Description: Build suffix automaton.
  * Time: O(n)
  */
+const int alpha = 26;
 
 struct state {
     int len, link;
-    map<char, int> next;
+    array<int, alpha> next;
 };
-
-const int MAXLEN = 100000;
 state st[MAXLEN * 2];
 int sz, last;
 
@@ -18,35 +17,43 @@ void sa_init() {
     sz = last = 0;
     st[0].len = 0;
     st[0].link = -1;
+    st[0].next.fill(-1);
     ++sz;
-    /*
-    // if you want to build an automaton for different strings:
-    for (int i=0; i<MAXLEN*2; ++i)
-            st[i].next.clear();
-    */
 }
 
-void sa_extend(char c) {
+int sa_cut(int v, int c) {
+    assert(st[v].next[c] != -1);
+    int to = st[v].next[c];
+    if (st[to].len == st[v].len + 1) {
+        return to;
+    }
+    int clone = sz++;
+    st[clone].len = st[v].len + 1;
+    st[clone].next = st[to].next;
+    st[clone].link = st[to].link;
+    for (; v != -1 && st[v].next[c] == to; v = st[v].link)
+        st[v].next[c] = clone;
+    st[to].link = clone;
+    return clone;
+}
+
+void sa_extend(int c) {
+    if (st[last].next[c] != -1) {
+        int to = sa_cut(last, c);
+        last = to;
+        return;
+    }
     int cur = sz++;
     st[cur].len = st[last].len + 1;
-    int p;
-    for (p = last; p != -1 && !st[p].next.count(c); p = st[p].link)
-        st[p].next[c] = cur;
-    if (p == -1)
+    st[cur].next.fill(-1);
+    int v;
+    for (v = last; v != -1 && st[v].next[c] == -1; v = st[v].link)
+        st[v].next[c] = cur;
+    if (v == -1)
         st[cur].link = 0;
     else {
-        int q = st[p].next[c];
-        if (st[p].len + 1 == st[q].len)
-            st[cur].link = q;
-        else {
-            int clone = sz++;
-            st[clone].len = st[p].len + 1;
-            st[clone].next = st[q].next;
-            st[clone].link = st[q].link;
-            for (; p != -1 && st[p].next[c] == q; p = st[p].link)
-                st[p].next[c] = clone;
-            st[q].link = st[cur].link = clone;
-        }
+        int to = st[v].next[c];
+        st[cur].link = sa_cut(v, c);
     }
     last = cur;
 }
