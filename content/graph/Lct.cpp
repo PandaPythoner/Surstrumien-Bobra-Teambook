3578125e016f79a6796bb77ab35fa1b0b5ec9409
Anton Stepanov <pandapythoner@gmail.com>
2025-12-14 23:59:51 +0300

    Changed link-cut

--- content/graph/Lct.cpp
+++ content/graph/Lct.cpp
@@ -1,143 +1,150 @@
 /**
- * Author: Alexander Nekrasov
+ * Author: Mangooste
  * Date: ?
  * Description: link-cut tree
  * Time: O(n\log(n))
  */
 
-#include <bits/stdc++.h> /** keep-include */
-using namespace std;
+struct link_cut {
+    struct node {
+        int par;
+        array<int, 2> sons;
+        bool inv;
+        int size;
 
-const int MAXN = 1e5 + 228;
+        node() : par(-1), sons({ -1, -1 }), inv(false), size(1) {}
+    };
 
-struct node {
-    node *ch[2];
-    node *p;
-    bool rev;
-    int sz;
+    vector<node> t;
 
-    node() {
-        ch[0] = ch[1] = p = NULL;
-        rev = false;
-        sz = 1;
+    int size() const {
+        return t.size();
+    }
+
+    void push(int v) {
+        if (t[v].inv) {
+            t[v].inv = false;
+            swap(t[v].sons[0], t[v].sons[1]);
+            for (const auto u : t[v].sons)
+                if (u != -1)
+                    t[u].inv ^= 1;
+        }
+    }
+
+    void relax(int v) {
+        push(v);
+        t[v].size = 1;
+        for (const auto x : t[v].sons)
+            if (x != -1)
+                t[v].size += t[x].size;
+    }
+
+    void rotate(int v) {
+        int u = t[v].par, w = t[u].par;
+        push(u), push(v);
+        t[v].par = w;
+        if (w != -1)
+            for (auto& x : t[w].sons)
+                if (x == u)
+                    x = v;
+        int i = t[u].sons[1] == v;
+        t[u].sons[i] = t[v].sons[i ^ 1];
+        if (t[v].sons[i ^ 1] != -1)
+            t[t[v].sons[i ^ 1]].par = u;
+        t[v].sons[i ^ 1] = u;
+        t[u].par = v;
+        relax(u), relax(v);
+    }
+
+    bool is_root(int v) const {
+        return t[v].par == -1 || (t[t[v].par].sons[0] != v && t[t[v].par].sons[1] != v);
+    }
+
+    void splay(int v) {
+        while (!is_root(v)) {
+            int u = t[v].par;
+            if (!is_root(u))
+                rotate((t[t[u].par].sons[0] == u) == (t[u].sons[0] == v) ? u : v);
+            rotate(v);
+        }
+        push(v);
+    }
+
+    int expose(int v) {
+        int prev = -1;
+        for (int u = v; u != -1; prev = u, u = t[u].par) {
+            splay(u);
+            t[u].sons[1] = prev;
+            relax(u);
+        }
+        splay(v);
+        assert(t[v].sons[1] == -1 && t[v].par == -1);
+        return prev;
+    }
+
+    link_cut(int n = 0) : t(n) {}
+
+    int add() {
+        t.push_back(node());
+        return int(t.size()) - 1;
+    }
+
+    void set_root(int root) {
+        expose(root);
+        t[root].inv ^= 1;
+        push(root);
+    }
+
+    bool connected(int v, int u) {
+        if (v == u)
+            return true;
+        expose(v), expose(u);
+        return t[v].par != -1;
+    }
+
+    bool link(int v, int u) {
+        if (connected(v, u))
+            return false;
+        t[u].inv ^= 1;
+        t[u].par = v;
+        expose(u);
+        return true;
+    }
+
+    bool cut(int v, int u) {
+        if (v == u)
+            return false;
+        set_root(v), expose(u);
+        if (t[u].sons[0] != v)
+            return false;
+        t[u].sons[0] = -1;
+        relax(u);
+        t[v].par = -1;
+        return true;
+    }
+
+    int par(int v, int root) {
+        if (!connected(v, root))
+            return -1;
+        set_root(root), expose(v);
+        if (t[v].sons[0] == -1)
+            return -1;
+        v = t[v].sons[0];
+        while (push(v), t[v].sons[1] != -1)
+            v = t[v].sons[1];
+        splay(v);
+        return v;
+    }
+
+    int distance(int v, int u) {
+        if (!connected(v, u))
+            return -1;
+        set_root(v), expose(u);
+        return t[u].sons[0] == -1 ? 0 : t[t[u].sons[0]].size;
+    }
+
+    int lca(int v, int u, int root) {
+        set_root(root), expose(v);
+        return expose(u);
     }
 };
-
-int getsz(node *n) { return (n == NULL) ? 0 : n->sz; }
-
-void pull(node *n) { n->sz = getsz(n->ch[0]) + getsz(n->ch[1]) + 1; }
-
-void push(node *n) {
-    if (n->rev) {
-        if (n->ch[0]) {
-            n->ch[0]->rev ^= 1;
-        }
-        if (n->ch[1]) {
-            n->ch[1]->rev ^= 1;
-        }
-        swap(n->ch[0], n->ch[1]);
-        n->rev = 0;
-    }
-}
-
-bool isRoot(node *n) {
-    return n->p == NULL || (n->p->ch[0] != n && n->p->ch[1] != n);
-}
-
-int chnum(node *n) { return n->p->ch[1] == n; }
-
-void attach(node *n, node *p, int num) {
-    if (n != NULL)
-        n->p = p;
-    if (p != NULL)
-        p->ch[num] = n;
-}
-
-void rotate(node *n) {
-    int num = chnum(n);
-    node *p = n->p;
-    node *b = n->ch[1 - num];
-    n->p = p->p;
-    if (!isRoot(p)) {
-        p->p->ch[chnum(p)] = n;
-    }
-    attach(p, n, 1 - num);
-    attach(b, p, num);
-    pull(p);
-    pull(n);
-}
-
-node *qq[MAXN];
-
-void splay(node *n) {
-    node *nn = n;
-    int top = 0;
-    qq[top++] = nn;
-    while (!isRoot(nn)) {
-        nn = nn->p;
-        qq[top++] = nn;
-    }
-    while (top) {
-        push(qq[--top]);
-    }
-    while (!isRoot(n)) {
-        if (!isRoot(n->p)) {
-            if (chnum(n) == chnum(n->p)) {
-                rotate(n->p);
-            } else {
-                rotate(n);
-            }
-        }
-        rotate(n);
-    }
-}
-
-void expose(node *n) {
-    splay(n);
-    n->ch[1] = NULL;
-    pull(n);
-    while (n->p != NULL) {
-        splay(n->p);
-        attach(n, n->p, 1);
-        pull(n->p);
-        splay(n);
-    }
-}
-
-void makeRoot(node *n) {
-    expose(n);
-    n->rev ^= 1;
-}
-
-node *nodes[MAXN];
-
-int main() {
-    int n;
-    cin >> n;
-    for (int i = 0; i <= n; i++) {
-        nodes[i] = new node();
-    }
-    int q;
-    cin >> q;
-    while (q--) {
-        string s;
-        cin >> s;
-        int u, v;
-        cin >> u >> v;
-        makeRoot(nodes[u]);
-        makeRoot(nodes[v]);
-        if (s == "get") {
-            if (isRoot(nodes[u]) && u != v) {
-                cout << "-1" << endl;
-            } else {
-                cout << getsz(nodes[v]) - 1 << endl;
-            }
-        } else if (s == "link") {
-            nodes[v]->p = nodes[u];
-        } else {
-            push(nodes[v]);
-            nodes[v]->ch[1] = NULL;
-            nodes[u]->p = NULL;
-        }
-    }
-}
